08967b39391a471bd9e279d3f266fa82eeda6a23
Tony <tony.beaujouan@etu.univ-orleans.fr>
2025-03-25 16:42:09 +0100

    Début du parse de la description et du détail de l'item

--- web/src/model/Item.js
+++ web/src/model/Item.js
@@ -16,7 +16,6 @@ export default class Item {
         console.log(`Description: ${this.plaintext}`);
         console.log(`Prix en Gold: ${this.gold}`);
         console.log(`Resource: ${this.partype}`);
-
     }
 
     async renderCard() {
@@ -40,5 +39,75 @@ export default class Item {
         h2.style = 'margin: 0px; text-align: center;';
         card.appendChild(h2);
         return card;
+    }
+    async renderDetail() {
+        const detail = document.createElement('div');
+        detail.classList.add('detail');
+
+        const divImage = document.createElement('div');
+        divImage.classList.add('image');
+        detail.appendChild(divImage);
+
+        const img = document.createElement('img');
+        img.classList.add('detail-image');
+        img.src = await Provider.getItemImageBase(this.image);
+        img.alt = this.name;
+        divImage.appendChild(img);
+
+        const divInfoBase = document.createElement('div');
+        divInfoBase.classList.add('info-base');
+        detail.appendChild(divInfoBase);
+
+        const h2 = document.createElement('h2');
+        h2.textContent = this.name;
+        h2.style = 'margin: 0px; text-align: center;';
+        divInfoBase.appendChild(h2);
+
+        divInfoBase.appendChild(this.parseDescription());
+
+        return detail;
+    }
+
+    replaceElementTag(oldElement, newTag) {
+        let newElement = document.createElement(newTag);
+
+        [...oldElement.attributes].forEach(attr => {
+            newElement.setAttribute(attr.name, attr.value);
+        });
+
+        while (oldElement.firstChild) {
+            newElement.appendChild(oldElement.firstChild);
+        }
+
+        oldElement.replaceWith(newElement);
+
+        return newElement;
+    }
+
+    replaceAllTags(parsedDoc, tagMapping) {
+        Object.entries(tagMapping).forEach(([oldTag, newTag]) => {
+            parsedDoc.querySelectorAll(oldTag).forEach(el => {
+                this.replaceElementTag(el, newTag);
+            });
+        });
+    }
+
+    parseDescription() {
+        let parser = new DOMParser();
+        let parsed = parser.parseFromString(this.description, "text/html");
+        this.replaceAllTags(parsed.body, {
+            "maintext": "div",
+            "stats": "p",
+            "attention": "b",
+            "passive": "p",
+            "healing": "b",
+            "onhit": "i"
+        });
+        console.log(parsed.body.firstChild);
+        return parsed.body.firstChild;
+    }
+
+    toString() {
+        return this.name;
     }
 }
